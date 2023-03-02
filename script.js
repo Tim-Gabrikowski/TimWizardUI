6b261ae33c64323fd1b04a36838db5fe304ea817
anton-tchekov <anton@tchekov.net>
2023-03-02 14:11:34 +0000

    Element IDs und Reihenfolge

--- script.js
+++ script.js
@@ -5,15 +5,9 @@ let touch_device = (('ontouchstart' in window) ||
 	(navigator.msMaxTouchPoints > 0));
 
 /* PAGE */
-function page_show(current) {
-	let pages = document.getElementsByClassName("page");
-	for(let page of pages) {
-		page.style.display = (page == current) ? "flex" : "none";
-	}
-}
-
 function page_create(options) {
 	let page = document.createElement("div");
+	_handle_id(page, options);
 	_handle_class(page, "page " + (options.center ? "center " : ""), options);
 	_handle_styles(page, options);
 	page.style.flexDirection = options.type || "column";
@@ -26,9 +20,17 @@ function page_create(options) {
 	return page;
 }
 
+function page_show(current) {
+	let pages = document.getElementsByClassName("page");
+	for(let page of pages) {
+		page.style.display = (page == current) ? "flex" : "none";
+	}
+}
+
 /* LAYOUT */
 function layout_create(options) {
 	let layout = document.createElement("div");
+	_handle_id(layout, options);
 	_handle_class(layout, "layout", options);
 	layout.style.flexDirection = options.type || "column";
 	_handle_styles(layout, options);
@@ -40,6 +42,7 @@ function layout_create(options) {
 function header_create(options) {
 	let header = document.createElement("p");
 	header.innerText = options.text || "HEADER";
+	_handle_id(header, options);
 	_handle_class(header, "header", options);
 	_handle_styles(header, options);
 	header.change = function(new_text) {
@@ -53,6 +56,7 @@ function header_create(options) {
 function text_create(options) {
 	let text = document.createElement("div");
 	text.innerText = options.text || "TEXT";
+	_handle_id(text, options);
 	_handle_class(text, "text", options);
 	_handle_styles(text, options);
 	text.change = function(new_text) {
@@ -67,6 +71,7 @@ function image_create(options) {
 	let image = document.createElement("img");
 	image.alt = options.alt || "";
 	image.src = options.src || "";
+	_handle_id(image, options);
 	_handle_class(image, "image", options);
 	_handle_styles(image, options);
 	return image;
@@ -75,6 +80,7 @@ function image_create(options) {
 /* BUTTON */
 function button_create(options) {
 	let button = document.createElement("button");
+	_handle_id(button, options);
 	_handle_class(button, "button", options);
 	if(options.label) {
 		button.innerText = options.label || "BUTTON";
@@ -111,6 +117,7 @@ function field_create(options) {
 	container.className = "input-container";
 
 	let input = document.createElement("input");
+	_handle_id(input, options);
 	_handle_class(input, "input", options);
 	input.value = options.value || "";
 	input.placeholder = options.placeholder || "";
@@ -140,6 +147,12 @@ function _handle_styles(element, options) {
 		for(let property in options.style) {
 			element.style[property] = options.style[property];
 		}
+	}
+}
+
+function _handle_id(element, options) {
+	if(options.id) {
+		element.id = options.id;
 	}
 }
 
